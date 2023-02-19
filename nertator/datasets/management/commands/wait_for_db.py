d40c40ebedc13847e5bdfc9e5bcfb02185a3828b
Hyan Batista <hyanbatista42@gmail.com>
2023-02-19 23:40:38 +0000

    feat: add wait for database command

---
+++ nertator/datasets/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+"""Django command to wait for the database to be available."""
+import time
+from typing import Any, Optional
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        """Entrypoint for command."""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
